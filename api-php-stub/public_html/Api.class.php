43741fa41cbf4e1ece32f802055a3e5ad18ae9f8
Ilya Tyumentsev <ilyatut@gmail.com>
2020-12-28 02:15:22 +0500

    PHP stub for the API

---
+++ api-php-stub/public_html/Api.class.php
@@ -0,0 +1,204 @@
+<?php
+
+class Api
+{
+    public $headers;
+    public $optionsHeaders;
+    public $apiPrefix;
+    public $method;
+    public $route;
+    public $token;
+    public $data;
+    public $routes;
+
+    /**
+     * Api constructor.
+     */
+    public function __construct()
+    {
+        # Set default options
+        $this->headers = [
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Headers' => 'Origin, X-Requested-With, Content-Type, Accept, api_key, Authorization',
+        ];
+        $this->optionsHeaders = [
+            'Access-Control-Allow-Methods' => 'GET, POST, DELETE, PUT, PATCH, OPTIONS',
+        ];
+        $this->apiPrefix = '/v1/';
+        $this->method = $_SERVER['REQUEST_METHOD'];
+        $this->route = str_replace($this->apiPrefix, '/', $_SERVER['REQUEST_URI']);
+        $this->token = $this->getBearerToken();
+        $this->data = $this->getData();
+        $this->routes = [];
+    }
+
+    /**
+     * Run the API
+     */
+    public function run()
+    {
+        $this->setHeaders();
+        $this->processRoutes();
+        $this->respond(404);
+    }
+
+    /**
+     * Set the provided headers
+     */
+    public function setHeaders()
+    {
+        foreach ($this->headers as $k => $v) {
+            header("{$k}: {$v}");
+        }
+        if ($this->method === 'OPTIONS') {
+            foreach ($this->optionsHeaders as $k => $v) {
+                header("{$k}: {$v}");
+            }
+            header("HTTP/1.1 200 OK");
+            die;
+        }
+    }
+
+    /**
+     * Process routes
+     */
+    public function processRoutes()
+    {
+        foreach ($this->routes as $route)
+        {
+            if ($this->method === $route['method'])
+            {
+                # Proecss simple routes
+                if (!$route['regexp'] && $this->route === $route['route']) {
+                    $route['callback']();
+                }
+
+                # Proecss regexp routes
+                if ($route['regexp'] && preg_match($route['route'], $this->route, $matches)) {
+                    $route['callback']( $matches );
+                }
+            }
+        }
+    }
+
+    /**
+     * Add a GET route
+     *
+     * @param string $route
+     * @param $callback
+     * @param bool $regexp
+     */
+    public function get($route, $callback, $regexp = false)
+    {
+        $this->routes[] = [
+            'method' => 'GET',
+            'route' => $route,
+            'callback' => $callback,
+            'regexp' => $regexp,
+        ];
+    }
+
+    /**
+     * Add a POST route
+     *
+     * @param string $route
+     * @param $callback
+     * @param bool $regexp
+     */
+    public function post($route, $callback, $regexp = false)
+    {
+        $this->routes[] = [
+            'method' => 'POST',
+            'route' => $route,
+            'callback' => $callback,
+            'regexp' => $regexp,
+        ];
+    }
+
+    /**
+     * Check if this is a not empty array
+     *
+     * @param $array
+     * @return bool
+     */
+    public static function ca($array) {
+        return is_array($array) && count($array) > 0;
+    }
+
+    /**
+     * Basic responses
+     *
+     * @param int $code
+     * @param array $data
+     * @param string $message
+     */
+    public function respond($code = 200, $data = [], $message = '')
+    {
+        if ($code === 200) {
+            header("HTTP/1.1 200 OK");
+            if (self::ca($data)) {
+                header("Content-Type: application/json");
+                echo json_encode($data);
+            }
+            die;
+        }
+        if ($code === 400) {
+            header("HTTP/1.1 400 Invalid request");
+            die;
+        }
+        if ($code === 401) {
+            header("HTTP/1.1 401 Unauthorised access");
+            die;
+        }
+        if ($code === 404) {
+            header("HTTP/1.1 404 Page not found");
+            die;
+        }
+        header("HTTP/1.1 {$code} {$message}");
+        die;
+    }
+
+    /**
+     * Set JSON data from the request body
+     */
+    private function getData()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+    
+    /** 
+     * Get header Authorization
+     */
+    private function getAuthorizationHeader()
+    {
+        $headers = null;
+        if (isset($_SERVER['Authorization'])) {
+            $headers = trim($_SERVER["Authorization"]);
+        }
+        elseif (isset($_SERVER['HTTP_AUTHORIZATION'])) {
+            $headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
+        }
+        elseif (function_exists('apache_request_headers')) {
+            $requestHeaders = apache_request_headers();
+            $requestHeaders = array_combine(
+                array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders)
+            );
+            if (isset($requestHeaders['Authorization'])) {
+                $headers = trim($requestHeaders['Authorization']);
+            }
+        }
+        return $headers;
+    }
+    
+    /**
+     * Get access token from header
+     */
+    private function getBearerToken()
+    {
+        $headers = $this->getAuthorizationHeader();
+        if (!empty($headers) && preg_match('/Bearer\s(\S+)/', $headers, $matches)) {
+            return $matches[1];
+        }
+        return null;
+    }
+}
---
+++ api-php-stub/public_html/index.php
@@ -0,0 +1,77 @@
+<?php
+
+# Import and init API
+require_once('Api.class.php');
+$api = new Api();
+
+
+# Secret credentials
+$user = [
+    'id' => 1,
+    'username' => 'test',
+    'password' => 'test',
+];
+
+$jwt_token = [
+    'access_token' => 'secret',
+    'access_type' => 'bearer',
+    'expires_in' => 3600,
+];
+
+
+# Main Page
+$api->get('/', function() use ($api) {
+    $api->respond(200, [
+        'status' => 'ok',
+        'message' => 'AG API v0.0.1',
+    ]);
+});
+
+# Register a New User
+$api->post('/user/register', function() use ($api, $jwt_token) {
+    if ($api->data['username'] && $api->data['password']) {
+        $api->respond(200, $jwt_token);
+    }
+    $api->respond(400);
+});
+
+# Login
+$api->post('/user/login', function() use ($api, $user, $jwt_token) {
+    if ($api->data['username'] === $user['username'] && $api->data['password'] === $user['password']) {
+        $api->respond(200, $jwt_token);
+    }
+    $api->respond(401);
+});
+
+# Logout
+$api->post('/user/logout', function() use ($api, $jwt_token) {
+    if ($api->token !== $jwt_token['access_token']) {
+        $api->respond(401);
+    }
+    $api->respond(200);
+});
+
+# Refresh JWT token
+$api->get('/user/refresh-token', function() use ($api, $jwt_token) {
+    if ($api->token !== $jwt_token['access_token']) {
+        $api->respond(401);
+    }
+    $api->respond(200, $jwt_token);
+});
+
+# Get User
+$api->get('|^/user/(\d+)$|', function($params) use ($api, $user, $jwt_token) {
+    if ($api->token !== $jwt_token['access_token']) {
+        $api->respond(401);
+    }
+    if ((int) $params[1] !== 1) {
+        $api->respond(404);
+    }
+    unset($user['password']);
+    $api->respond(200, $user);
+
+}, true);
+
+
+# Run the API
+$api->run();
