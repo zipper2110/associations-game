8ed8504c2521dbb02effbb3d576bb1f2868c6f86
Dmitri Litvin <ext.dlitvin@tinkoff.ru>
2021-01-17 14:33:25 +0300

    более удобный тестовый UI

--- backend-kotlin-spring/src/main/resources/static/app.js
+++ backend-kotlin-spring/src/main/resources/static/app.js
@@ -1,5 +1,7 @@
 var stompClient = null;
 
+let userToken = null
+
 function setConnected(connected) {
     $("#connect").prop("disabled", connected);
     $("#disconnect").prop("disabled", !connected);
@@ -18,6 +20,7 @@ function connect() {
     stompClient.connect({}, function (frame) {
         setConnected(true);
         console.log('Connected: ' + frame);
+        $(".websocket-status").text("подключён")
         stompClient.subscribe('/topic/*', function (greeting) {
             console.log(greeting.body);
             showGreeting(greeting.body);
@@ -31,15 +34,80 @@ function disconnect() {
     }
     setConnected(false);
     console.log("Disconnected");
+    $(".websocket-status").text("отключён")
 }
 
 function sendCommand() {
-    stompClient.send($("#path").val(), {}, $("#content").val());
-    // stompClient.send("/game/auth", {}, JSON.stringify({'name': $("#name").val()}));
+    let message = JSON.parse($("#content").val())
+    message.token = userToken
+    stompClient.send("/game/" + $("#path").val(), {}, JSON.stringify(message));
 }
 
 function showGreeting(message) {
     $("#greetings").append("<tr><td>" + message + "</td></tr>");
+}
+
+function logIn() {
+    let username = $("#username").val();
+    fetch("/auth", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({username })
+    }).then(response => {
+        response.json().then(json => {
+            let status = "успешно вошли"
+            if (response.status !== 200) {
+                status = "не удалось войти"
+            } else {
+                setToken(json.token)
+            }
+            $("#auth-status").text(status)
+            log("авторизация", status)
+        })
+    })
+}
+
+function logOut() {
+    fetch("/logout", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ token: userToken })
+    }).then(response => {
+        let status
+        if (response.status === 200) {
+            status = "вышли"
+            setToken("")
+        } else {
+            status = "ошибка при выходе"
+        }
+        $("#auth-status").text(status)
+        log("авторизация", status)
+    })
+}
+
+function getUser() {
+    fetch("/me?token=" + userToken).then(response => response.json().then( json => {
+        let user
+        if (response.status !== 200) {
+            user = "ошибка"
+        } else {
+            user = JSON.stringify(json, null, 2)
+        }
+        $("#user-info").text(user)
+    }))
+}
+
+function log(source, message) {
+    $(".websocket-log").add(`<p>${source + ": " + message}</p>`)
+}
+
+function setToken(token) {
+    userToken = token
+    $("#token").val(userToken)
 }
 
 $(function () {
@@ -49,4 +117,7 @@ $(function () {
     $( "#connect" ).click(function() { connect(); });
     $( "#disconnect" ).click(function() { disconnect(); });
     $( "#send-command" ).click(function() { sendCommand(); });
+    $( "#login" ).click(function() { logIn(); });
+    $( "#logout" ).click(function() { logOut(); });
+    $( "#get-user" ).click(function() { getUser(); });
 });
